de234c54f79063e4e46e8f4a4caf199848861619
marcuspramirez <@macusramirez@gmail.com>
2020-12-07 17:52:42 -0600

    reinbursements posting

--- public/js/reinbursementScript.js
+++ public/js/reinbursementScript.js
@@ -3,20 +3,23 @@ $(document).ready(function () {
 
         $("#applyBtn").click(function (event) {
             event.preventDefault();
-            // console.log($('.box').find(":selected").text());
-            // console.log($('.donation-amount').find(":selected").text());
+            // console.log($('.school').val());
+            // console.log($('.course').val());
+            // console.log($('.grade').find(":selected").text());
+            // console.log($('.classMessage').val());
+            
             var user = "user@example.com"
-            var school = "SMU"
-            var course = "Biology"
-            var grade = "A"
-            var message = "hard coded message"
+            var school = $('.school').val()
+            var course = $('.course').val()
+            var grade = $('.grade').find(":selected").text()
+            var message = $('.classMessage').val()
                 $.post("/api/posts/reinburse", {user, school, course, grade, message}
                 )
                     .then(function(){
                         console.log("Added Charity");
                     });
 
-        });
+         });
     });
 
 
